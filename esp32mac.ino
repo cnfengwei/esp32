db08b4e36c593c35925b4a4ce9129fd8108ccd92
wffengwei <44801138+cnfengwei@users.noreply.github.com>
2025-10-15 06:34:24 +0800

    Update esp32mac.ino
    更改为3楼打卡mac地址

--- esp32mac.ino
+++ esp32mac.ino
@@ -5,13 +5,13 @@
 
 
 
-uint8_t newMACAddress[] = {0xF8, 0x8C, 0x21, 0x2A, 0x94, 0x07};
+uint8_t newMACAddress[] = {0x68, 0xdd, 0xb7, 0x4a, 0x23, 0xff};
 
 void setup() {
   // put your setup code here, to run once:
   WiFi.mode(WIFI_AP);//加入wifi模式，以便后面的setmac函数修改地址
   Serial.begin(115200);   // 设置波特率
-  WiFi.softAP("suboshipin","subo8888");
+  WiFi.softAP("3lou","subosubo");
   Serial.println();
   Serial.print("ESP的MAC地址:  ");
   Serial.println(WiFi.macAddress());
